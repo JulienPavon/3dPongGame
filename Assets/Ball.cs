bfc74d3e788551dd42ce1f5ff8020466579efe2a
Julien Pavon <julien.pavon@gmail.com>
2016-07-05 14:59:31 +0200

    Main scene using few scripts

---
+++ Assets/Ball.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+
+	private bool isStarted;
+	private Rigidbody rigidBody;
+	private Vector3 initialPosition;
+	private const float velocityZ = 6f;
+
+	// Use this for initialization
+	void Start () {
+		rigidBody = GetComponent<Rigidbody>();
+		initialPosition = rigidBody.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!isStarted && Input.GetKey(KeyCode.Space)) {
+			isStarted = true;
+
+			var velocity = new Vector3 (Random.Range (-4, 4), 0, velocityZ);
+			rigidBody.velocity = velocity;
+		}
+	}
+
+	void OnCollisionEnter(Collision collision) {
+		var name = collision.gameObject.name;
+
+		switch (name) {
+		case "BorderLeft":
+		case "BorderRight":
+			Reset ();
+			break;
+		}
+	}
+
+	private void Reset() {
+		rigidBody.transform.position = initialPosition;
+		rigidBody.velocity = Vector3.zero;
+		isStarted = false;
+	}
+}
---
+++ Assets/Paddle.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Paddle : MonoBehaviour {
+
+	public enum Player {
+		Player1,
+		Player2
+	}
+
+	public Player player;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		switch (player) {
+		case Player.Player1:
+			if (Input.GetKey (KeyCode.A)) {
+				if (CanMoveUp ()) {
+					MoveUp ();
+				}
+			}
+			if (Input.GetKey (KeyCode.Q)) {
+				if (CanMoveDown ()) {
+					MoveDown ();
+				}
+			}
+			break;
+		case Player.Player2:
+			if (Input.GetKey (KeyCode.UpArrow)) {
+				if (CanMoveUp ()) {
+					MoveUp ();
+				}
+			}
+			if (Input.GetKey (KeyCode.DownArrow)) {
+				if (CanMoveDown ()) {
+					MoveDown ();
+				}
+			}
+			break;
+		}
+	}
+
+	private bool CanMoveUp () {
+		return transform.position.x < 5;
+	}
+
+	private bool CanMoveDown () {
+		return transform.position.x > -5;
+	}
+
+	private void MoveUp() {
+		transform.Translate (new Vector3 (0.3f, 0, 0));
+	}
+
+	private void MoveDown() {
+		transform.Translate (new Vector3 (-0.3f, 0, 0));
+	}
+}
